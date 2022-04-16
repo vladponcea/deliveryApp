65b01c28858f6fba1c7f2c9b895d4872be7acac2
vladponcea <vladponcea@icloud.com>
2022-04-16 13:50:55 +0300

    test

---
+++ deliveryApp/deliveryApp/Views/RestaurantView.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantView.swift
+//  deliveryApp
+//
+//  Created by Vladut Mihai Poncea on 16.04.2022.
+//
+
+import SwiftUI
+
+struct RestaurantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RestaurantView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestaurantView()
+    }
+}
